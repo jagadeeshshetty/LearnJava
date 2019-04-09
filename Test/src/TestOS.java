7d3b84f4734bad9fbc1f98f647a15acee6e39bf1
Jagadeesh from macOS <jagadeesh.cheluvaraj@gmail.com>
2019-04-09 07:46:02 +0530

    Added TestOS.java file.

---
+++ Test/src/TestOS.java
@@ -0,0 +1,12 @@
+
+public class TestOS {
+
+	public static void main(String[] args) {
+
+		System.out.println("*** Program starts ***");
+
+		System.out.println("*** Program completes ***");
+
+	}
+
+}
