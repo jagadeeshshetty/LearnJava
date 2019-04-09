9e43e2ccc173b82fef681a5ec2689655df6fbbea
Jagadeesh Shetty <jagadeesh.cheluvaraj@gmail.com>
2019-04-09 16:11:08 +0530

    Updated TestOS class on windows platform.

--- Test/src/TestOS.java
+++ Test/src/TestOS.java
@@ -3,20 +3,22 @@ public class TestOS {
 
 	public static void main(String[] args) {
 
-		System.out.println("*** Program starts ***");
+		System.out.println("*** PROGRAM STARTS ***\n");
 
 		// List all the properties.
 		// System.getProperties().list(System.out);
 
 		String osName = System.getProperty("os.name");
 
-		if (osName.equalsIgnoreCase("Mac OS X")) {
-			System.out.println(System.getProperty("os.name"));
+		if ( osName.equalsIgnoreCase("Mac OS X") ) {
+			System.out.println( System.getProperty("os.name") );
+		} else if ( osName.equalsIgnoreCase("Windows 10") ) {
+			System.out.println( System.getProperty("os.name") );
 		} else {
 			System.out.println("OS not identified.");
 		}
 
-		System.out.println("*** Program completes ***");
+		System.out.println("\n*** PROGRAM COMPLETES ***");
 
 	}
 
