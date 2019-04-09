fcf58d6b35081604f91abd438e2c2f8d8047bf68
Jagadeesh Shetty <jagadeesh.cheluvaraj@gmail.com>
2019-04-09 16:31:25 +0530

    added CoreJavaBasics project from windows platform.

---
+++ CoreJavaBasics/src/main/java/com/arrays/Test.java
@@ -0,0 +1,13 @@
+package com.arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("*** PROGRAM STARTS ***\n");
+
+		System.out.println("\n*** PROGRAM COMPLETES ***");
+
+	}
+
+}
