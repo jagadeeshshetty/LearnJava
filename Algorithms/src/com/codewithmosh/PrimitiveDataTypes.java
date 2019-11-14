427421a25fd05f6dc8f1d15bb99d5f89c71b9931
JC <jagadeesh@rebaca.com>
2019-11-14 22:05:42 +0530

    learning primitive data types.

---
+++ Algorithms/src/com/codewithmosh/PrimitiveDataTypes.java
@@ -0,0 +1,15 @@
+package com.codewithmosh;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte age = 30;
+        int millionViewsCount = 123_456_789;
+        long billionViewsCount = 3_123_456_789L;
+        float temperature = 4.8F;
+        double price = 2.4;
+        char letter = 'A';
+        boolean isEligible = true;
+
+        System.out.printf(String.valueOf(billionViewsCount));
+    }
+}
