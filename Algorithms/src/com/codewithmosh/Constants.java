bb4f173d257af2838efcebb9f780158fae61a3a7
JC <jagadeesh.cheluvaraj@gmail.com>
2019-11-21 20:42:20 +0530

    basics: learning constants

---
+++ Algorithms/src/com/codewithmosh/Constants.java
@@ -0,0 +1,11 @@
+package com.codewithmosh;
+
+public class Constants {
+    public static void main(String[] args) {
+        // Again we can't assign value to variable declared with
+        // final access modifier
+        final float PI = 3.14F;
+
+        System.out.println(String.format("PI: %f", PI));
+    }
+}
