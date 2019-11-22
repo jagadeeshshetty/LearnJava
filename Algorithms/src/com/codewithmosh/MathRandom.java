19ff81c672c583fa1977387a0c8766af629a3816
JC <jagadeesh@rebaca.com>
2019-11-22 15:12:16 +0530

    basics: learning Math inbuilt methods.

---
+++ Algorithms/src/com/codewithmosh/MathRandom.java
@@ -0,0 +1,8 @@
+package com.codewithmosh;
+
+public class MathRandom {
+    public static void main(String[] args) {
+        int result = (int) (Math.random() * 100);
+        System.out.println(result);
+    }
+}
